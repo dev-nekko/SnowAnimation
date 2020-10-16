f3ee41945680f85618f6799cb56c318837e5561a
xfrogz <renattoyague@gmail.com>
2020-10-16 01:31:10 -0500

    Test Balloons

--- SnowFlake.js
+++ SnowFlake.js
@@ -1,34 +1,32 @@
 function mapNumber(num, min, max, newMin, newMax) {
-  const normal = (num - min)/(max - min);
-  return normal*(newMax - newMin) + newMin;
+  const normal = (num - min) / (max - min);
+  return normal * (newMax - newMin) + newMin;
 }
 
 class SnowFlake {
   constructor(opts) {
-    this.width = 960;
-    this.height = 960;
+    this.width = 1200;
+    this.height = 200;
 
     this.position = createVector(
       mapNumber(random(), 0, 1, 0, this.width),
       mapNumber(random(), 0, 1, 0, this.height)
-    )
-    
+    );
+
     this.image = opts.image;
     this.size = opts.size;
     this.Xoffset = mapNumber(random(), 0, 1, 0, this.width);
     this.Yoffset = mapNumber(random(), 0, 1, 0, this.height);
   }
-
 }
-
 
 SnowFlake.prototype.draw = function () {
   image(this.image, this.position.x, this.position.y, this.size, this.size);
-  this.position.x += mapNumber(noise(this.Xoffset), 0, 1, -5, 5);
-  this.position.y += mapNumber(noise(this.Yoffset), 0, 1, 0, 5);
+  this.position.x += mapNumber(noise(this.Xoffset), 1, 0, 0, 2);
+  this.position.y += mapNumber(noise(this.Yoffset), 1, 0, -2, 2);
   this.Xoffset += 0.0001;
   this.Yoffset += 0.0001;
-}
+};
 
 SnowFlake.prototype.inScreen = function () {
   if (this.position.x < 0 || this.position.x > this.width) {
@@ -38,4 +36,4 @@ SnowFlake.prototype.inScreen = function () {
     return false;
   }
   return true;
-}
+};
--- sketch.js
+++ sketch.js
@@ -3,27 +3,25 @@ let snowflakes = [];
 let bg;
 
 function setup() {
-	createCanvas(960, 960);
-	img = loadImage("Snow.png");
-	bg = loadImage("ccat.jpg");
-	image(bg, 0, 0);
-	for (let i = 0; i < 1000; i++) {
-		snowflakes.push(new SnowFlake({ image: img, size: 15 }));
-	}
-
-
+  createCanvas(2040, 2040);
+  img = loadImage("balloons.png");
+  bg = loadImage("Adathink.png");
+  image(bg, 0, 0);
+  for (let i = 0; i < 50; i++) {
+    snowflakes.push(new SnowFlake({ image: img, size: 70 }));
+  }
 }
 
 function draw() {
-	background(255);
-	image(bg, 0, 0);
-	console.log(bg.width, bg.height);
-	for (let i = 0; i < snowflakes.length; i++) {
-		let snow = snowflakes[i];
-		if (snow.inScreen()) {
-			snow.draw();
-		} else {
-			snowflakes[i] = new SnowFlake({ image: img, size: 15 });
-		}
-	}
+  background(255);
+  image(bg, 0, 0);
+  console.log(bg.width, bg.height);
+  for (let i = 0; i < snowflakes.length; i++) {
+    let snow = snowflakes[i];
+    if (snow.inScreen()) {
+      snow.draw();
+    } else {
+      snowflakes[i] = new SnowFlake({ image: img, size: 70 });
+    }
+  }
 }
